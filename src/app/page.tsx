c1faa59ff89fa460183f8b1d3d232d181ed5a2e5
Helge <elitaer@gmail.com>
2023-03-10 10:43:59 +0100

    formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,6 +56,7 @@ function Main() {
           </div>
         </div>
       </section>
+
       <section className='px-8 lg:mx-32 lg:px-0'>
         <TextStage className='relative z-10 -mt-56 lg:-mt-14'>
           <p className='w-full max-w-[60ch] px-2 lg:max-w-full lg:px-32'>
